5f367f4af610b2b1291bf4bbc05a032a083639fc
Dave Williams <davejjwilliams@gmail.com>
2024-04-22 23:42:29 +0100

    Navbar appears on scroll

--- index.js
+++ index.js
@@ -2,3 +2,31 @@ function elementScroll(param) {
   const element = document.getElementById(param);
   element.scrollIntoView();
 }
+
+$(window).on('load', function () {
+  mainNav();
+  $(window).scroll(function () {
+    mainNav();
+  });
+  function mainNav() {
+    var top =
+      (document.documentElement && document.documentElement.scrollTop) ||
+      document.body.scrollTop;
+    if (top > 40)
+      $('.navbar-fixed-top').stop().animate(
+        {
+          opacity: '1',
+          top: '0'
+        },
+        0
+      );
+    else
+      $('.navbar-fixed-top').stop().animate(
+        {
+          top: '-70',
+          opacity: '0'
+        },
+        0
+      );
+  }
+});
